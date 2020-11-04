4441fde6e88a9d7683b3840ddcd5d9c9ab7b4987
Hmax3J <Hmax3J@gamil.com>
2020-11-04 16:07:10 +0900

    7

---
+++ SortEx7/src/sortex7/SortEx7.java
@@ -0,0 +1,45 @@
+package sortex7;
+import java.io.*;
+import java.util.*;
+
+//앞에 나온 예제 대부분과 지금 하고 있는 정렬 예제는 https://st-lab.tistory.com/111?category=857114 에서 참고 했습니다. 
+//실력이 아직 많이 미숙하여 소스코드를 보고 재수정은 못해봤지만 실력을 키운뒤에 재수정을 해 볼 예정입니다.
+
+public class SortEx7 {
+
+	public static void main(String[] args) throws IOException 
+	{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] arr = new int[N][2];
+		
+		for(int i = 0; i < N; i++) 
+		{
+			// 위치 주의
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr, (e1, e2) -> {
+			if(e1[0] == e2[0]) {
+				return e1[1] - e2[1];
+			} else {
+				return e1[0] - e2[0];
+			}
+		});
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0 ; i< N ; i++) 
+		{
+			// 위치 주의
+			sb.append(arr[i][1] + " " + arr[i][0]).append('\n');
+		}
+		System.out.println(sb);
+
+	}
+
+}
